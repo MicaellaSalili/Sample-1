3410ad0220e3536920cc083a9a3f6e364832e3d9
heiimics <70866584+MicaellaSalili@users.noreply.github.com>
2024-12-08 16:34:36 +0800

    chore: add initial project files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="centered-text" style={{ textAlign: 'left' }}>
+        SAMPLE 1<br />
+        sample-1
+      </div>
+    </div>
+  );
+}
+
+export default App;
